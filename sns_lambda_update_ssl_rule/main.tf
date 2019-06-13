f7ee11159baaeabfc83132ba80be98ba5f3975fa
Leonardo Zavala (Leo) <lezavala@torchmarkcorp.com>
2019-06-13 14:39:39 -0700

    Fixes ternary

--- sns_lambda_update_ssl_rule/main.tf
+++ sns_lambda_update_ssl_rule/main.tf
@@ -17,13 +17,13 @@ module "lambda_label" {
 data "aws_sns_topic" "this" {
   count = "${(1 - var.create_sns_topic) * var.create}"
 
-  name = "${var.sns_topic_name ? var.sns_topic_name : module.sns_topic_label.id}"
+  name = "${var.sns_topic_name == "" ? module.sns_topic_label.id :  var.sns_topic_name}"
 }
 
 resource "aws_sns_topic" "this" {
   count = "${var.create_sns_topic * var.create}"
 
-  name = "${var.sns_topic_name ? var.sns_topic_name : module.sns_topic_label.id}"
+  name = "${var.sns_topic_name == ""?  module.sns_topic_label.id :  var.sns_topic_name}"
 }
 
 locals {
