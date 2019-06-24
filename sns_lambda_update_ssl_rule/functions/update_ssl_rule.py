51265a8d5c372ea0a192d1448c18f41f69863677
Leonardo Zavala (Leo) <lezavala@torchmarkcorp.com>
2019-06-24 11:07:34 -0700

    Fixes Lambda function.

--- sns_lambda_update_ssl_rule/functions/update_ssl_rule.py
+++ sns_lambda_update_ssl_rule/functions/update_ssl_rule.py
@@ -5,10 +5,13 @@ def lambda_handler(event, context):
     region = os.environ['ELB_REGION']
     elbv2_client = boto3.client('elbv2', region_name=region)
 
+    available_target_groups = os.environ['AVAILABLE_TARGET_GROUPS']
+    arrMatches = available_target_groups.split(',')
+
     # Get HTTP Target Group.
     http_listener_arn = os.environ['PRODUCTION_LISTENER_ARN']
     http_listener = elbv2_client.describe_rules( ListenerArn=http_listener_arn)
-    http_target_group_arn = get_current_http_target_group(https_listener['Rules'])
+    http_target_group_arn = get_current_http_target_group(http_listener['Rules'], arrMatches)
 
     # Get HTTPS listener rules.
     https_listener_arn = os.environ['SSL_LISTENER_ARN']
@@ -45,15 +48,13 @@ def lambda_handler(event, context):
     return results
 
 # Returns the current B/G target group from a list of lister rules.
-def get_current_http_target_group(http_listener_rules):
-    available_target_groups = os.environ['AVAILABLE_TARGET_GROUPS']
-    arrMatches = available_target_groups.split(',')
+def get_current_http_target_group(http_listener_rules, arrMatches):
 
     i=0
     while i < len(http_listener_rules):
 
         # Continue if default listener rule.
-        if https_listener_rules[i]['IsDefault']==True:
+        if http_listener_rules[i]['IsDefault']==True:
             i +=1
             continue
 
